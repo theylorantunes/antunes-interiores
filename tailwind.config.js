2a46a8f6bee3eb2187269f1640c0ed23157aed8d
theylorantunes <theylorantunes@gmail.com>
2025-12-30 00:14:01 -0300

    feat: configurações personalizadas do framework

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./index.html", "./**/*.{html,js}"],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+
+      fontFamily: {
+
+        'display': ['Playfair Display', 'serif'],
+        'cormorant': ['Cormorant Garamond', 'serif'],
+        'lato': ['Lato', 'system-ui', 'sans-serif'],
+
+      },
+
+      colors:{
+
+        'headerbg': '#FDFBF7',
+        'headerbgalt': '#F2EFE9',
+        'maintext': '#4A4A4A',
+        'cta-gold': '#A68A64',
+        'cta-gold-hover': '#8C9387',
+        'borders-shadow': '#E7E2D6'
+      },
+
+      backgroundImage: {
+        'hero':'url("../img/hero-bg.webp")',
+      }
+    },
+    plugins: [],
+  }
 }
 
