51053d0c7579f84c903bb282fbfdaae426ebf99f
theylorantunes <theylorantunes@gmail.com>
2026-01-15 11:38:19 -0300

    fix: mudando de 2 para 3 slides no desktop, mas mantendo 2 no mobile

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -150,6 +150,10 @@ document.addEventListener('DOMContentLoaded', () => {
             breakpoints: {
                 768: {
                     slidesPerView: 2,
+                    spaceBetween: 30,
+                },
+                1024: {
+                    slidesPerView: 3,
                     spaceBetween: 40,
                 }
             }
